ec5272edd7683dba896253ae7865259c4e0112de
han yong <hanyongmvp@163.com>
2024-06-22 17:44:43 +0800

    111

--- dify-main/api/extensions/storage/oci_storage.py
+++ dify-main/api/extensions/storage/oci_storage.py
@@ -1,52 +1,41 @@
 from collections.abc import Generator
 from contextlib import closing
-
-import oci
 from flask import Flask
-
+from botocore.client import Config
+from botocore.exceptions import ClientError
 from extensions.storage.base_storage import BaseStorage
+import boto3
 
 
 class OCIStorage(BaseStorage):
-    """Implementation for OCI storage.
+    """Implementation for s3 storage.
     """
     def __init__(self, app: Flask):
         super().__init__(app)
         app_config = self.app.config
-        self.bucket_name = app_config.get('OCI_BUCKET_NAME')
-        
-        config = {
-            "user": app_config.get('OCI_USER_OCID'),
-            "key_file": app_config.get('OCI_KEY_FILE'),
-            "fingerprint": app_config.get('OCI_FINGERPRINT'),
-            "tenancy": app_config.get('OCI_TENANCY_OCID'),
-            "region": app_config.get('OCI_REGION')
-        }
-
-        self.client = oci.object_storage.ObjectStorageClient(config)
-        self.namespace = self.client.get_namespace().data
+        self.bucket_name = app_config.get('S3_BUCKET_NAME')
+        if app_config.get('S3_USE_AWS_MANAGED_IAM'):
+            session = boto3.Session()
+            self.client = session.client('s3')
+        else:
+            self.client = boto3.client(
+                        's3',
+                        aws_secret_access_key=app_config.get('S3_SECRET_KEY'),
+                        aws_access_key_id=app_config.get('S3_ACCESS_KEY'),
+                        endpoint_url=app_config.get('S3_ENDPOINT'),
+                        region_name=app_config.get('S3_REGION'),
+                        config=Config(s3={'addressing_style': app_config.get('S3_ADDRESS_STYLE')})
+                    )
 
     def save(self, filename, data):
-        obj = oci.object_storage.models.PutObjectDetails()
-        obj.put_object_body = data
-        self.client.put_object(
-            namespace_name=self.namespace,
-            bucket_name=self.bucket_name,
-            object_name=filename,
-            put_object_body=data
-        )
+        self.client.put_object(Bucket=self.bucket_name, Key=filename, Body=data)
 
     def load_once(self, filename: str) -> bytes:
         try:
             with closing(self.client) as client:
-                response = client.get_object(
-                    namespace_name=self.namespace,
-                    bucket_name=self.bucket_name,
-                    object_name=filename
-                )
-                data = response.data.content
-        except oci.exceptions.ServiceError as ex:
-            if ex.status == 404:
+                data = client.get_object(Bucket=self.bucket_name, Key=filename)['Body'].read()
+        except ClientError as ex:
+            if ex.response['Error']['Code'] == 'NoSuchKey':
                 raise FileNotFoundError("File not found")
             else:
                 raise
@@ -56,14 +45,10 @@ class OCIStorage(BaseStorage):
         def generate(filename: str = filename) -> Generator:
             try:
                 with closing(self.client) as client:
-                    response = client.get_object(
-                        namespace_name=self.namespace,
-                        bucket_name=self.bucket_name,
-                        object_name=filename
-                    )
-                    yield from response.data.raw.stream(1024)
-            except oci.exceptions.ServiceError as ex:
-                if ex.status == 404:
+                    response = client.get_object(Bucket=self.bucket_name, Key=filename)
+                    yield from response['Body'].iter_chunks()
+            except ClientError as ex:
+                if ex.response['Error']['Code'] == 'NoSuchKey':
                     raise FileNotFoundError("File not found")
                 else:
                     raise
@@ -71,33 +56,15 @@ class OCIStorage(BaseStorage):
 
     def download(self, filename, target_filepath):
         with closing(self.client) as client:
-            response = client.get_object(
-                namespace_name=self.namespace,
-                bucket_name=self.bucket_name,
-                object_name=filename
-            )
-            with open(target_filepath, 'wb') as file:
-                for chunk in response.data.raw.stream(1024):
-                    file.write(chunk)
+            client.download_file(self.bucket_name, filename, target_filepath)
 
     def exists(self, filename):
         with closing(self.client) as client:
             try:
-                client.head_object(
-                    namespace_name=self.namespace,
-                    bucket_name=self.bucket_name,
-                    object_name=filename
-                )
+                client.head_object(Bucket=self.bucket_name, Key=filename)
                 return True
-            except oci.exceptions.ServiceError as ex:
-                if ex.status == 404:
-                    return False
-                else:
-                    raise
+            except:
+                return False
 
     def delete(self, filename):
-        self.client.delete_object(
-            namespace_name=self.namespace,
-            bucket_name=self.bucket_name,
-            object_name=filename
-        )
+        self.client.delete_object(Bucket=self.bucket_name, Key=filename)
